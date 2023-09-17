3a74a4f5bb4f496ec9221d6d343fc708638e94a8
Akito Koga <akito121215@gmail.com>
2023-09-17 12:23:15 +0900

    add: Add AWS provider

---
+++ environments/dev/main.tf
@@ -0,0 +1,19 @@
+# ---------------------------------------
+# Terraform Configuration
+# ---------------------------------------
+terraform {
+  required_version = ">=1.4.4"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">3.0"
+    }
+  }
+
+  # Store tfstate in Amazon S3
+  backend "s3" {
+    bucket = "terraform-tfstate-sample-dev"
+    key    = "ap-northeast-1/terraform.tfstate"
+    region = "ap-northeast-1"
+  }
+}
