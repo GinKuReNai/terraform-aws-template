a9aeea25065394f604b2afed2038054da76690d1
GinKuReNai <akito121215@gmail.com>
2023-09-23 22:26:19 +0900

    fix: fix security group settings

--- modules/network/securitygroup.tf
+++ modules/network/securitygroup.tf
@@ -11,6 +11,7 @@ resource "aws_vpc_security_group_ingress_rule" "alb_sg_ingress_rule" {
   from_port = 80
   to_port = 80
   ip_protocol = "tcp"
+  cidr_ipv4 = "0.0.0.0/0"
   tags = {
     Name = "${var.project}-${var.environment}-alb-sg-ingress-rule"
   }
@@ -24,11 +25,23 @@ resource "aws_security_group" "ecs_sg" {
   vpc_id = aws_vpc.vpc.id
 }
 
-resource "aws_vpc_security_group_ingress_rule" "ecs_sg_ingress_rule" {
+resource "aws_vpc_security_group_ingress_rule" "ecs_sg_ingress_rule_1a" {
   security_group_id = aws_security_group.ecs_sg.id
   from_port = 80
   to_port = 80
   ip_protocol = "tcp"
+  cidr_ipv4 = var.cidr_block.public_subnet_ingress_1a
+  tags = {
+    Name = "${var.project}-${var.environment}-ecs-sg-ingress-rule"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ecs_sg_ingress_rule_1c" {
+  security_group_id = aws_security_group.ecs_sg.id
+  from_port = 80
+  to_port = 80
+  ip_protocol = "tcp"
+  cidr_ipv4 = var.cidr_block.public_subnet_ingress_1c
   tags = {
     Name = "${var.project}-${var.environment}-ecs-sg-ingress-rule"
   }
@@ -42,12 +55,25 @@ resource "aws_security_group" "vpc_endpoint_sg" {
   vpc_id = aws_vpc.vpc.id
 }
 
-resource "aws_vpc_security_group_ingress_rule" "vpc_endpoint_sg_ingress_rule" {
+resource "aws_vpc_security_group_ingress_rule" "vpc_endpoint_sg_ingress_rule_1a" {
   security_group_id = aws_security_group.vpc_endpoint_sg.id
   # ECR operations are performed using HTTPS
   from_port = 443
   to_port = 443
   ip_protocol = "tcp"
+  cidr_ipv4 = var.cidr_block.private_subnet_application_1a
+  tags = {
+    Name = "${var.project}-${var.environment}-vpc-endpoint-sg-ingress-rule"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "vpc_endpoint_sg_ingress_rule_1c" {
+  security_group_id = aws_security_group.vpc_endpoint_sg.id
+  # ECR operations are performed using HTTPS
+  from_port = 443
+  to_port = 443
+  ip_protocol = "tcp"
+  cidr_ipv4 = var.cidr_block.private_subnet_application_1c
   tags = {
     Name = "${var.project}-${var.environment}-vpc-endpoint-sg-ingress-rule"
   }
@@ -56,17 +82,30 @@ resource "aws_vpc_security_group_ingress_rule" "vpc_endpoint_sg_ingress_rule" {
 # ---------------------------------------
 # Security Group(RDS:Aurora MySQL)
 # ---------------------------------------
-resource "aws_security_group" "rds-sg" {
+resource "aws_security_group" "rds_sg" {
   name = "${var.project}-${var.environment}-rds-sg"
   vpc_id = aws_vpc.vpc.id
 }
 
-resource "aws_vpc_security_group_ingress_rule" "rds_sg_ingress_rule" {
-  security_group_id = aws_security_group.rds-sg.id
+resource "aws_vpc_security_group_ingress_rule" "rds_sg_ingress_rule_1a" {
+  security_group_id = aws_security_group.rds_sg.id
   # Aurora MySQL port is 3306
   from_port = 3306
   to_port = 3306
   ip_protocol = "tcp"
+  cidr_ipv4 = var.cidr_block.private_subnet_application_1a
+  tags = {
+    Name = "${var.project}-${var.environment}-rds-sg-ingress-rule"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "rds_sg_ingress_rule_1c" {
+  security_group_id = aws_security_group.rds_sg.id
+  # Aurora MySQL port is 3306
+  from_port = 3306
+  to_port = 3306
+  ip_protocol = "tcp"
+  cidr_ipv4 = var.cidr_block.private_subnet_application_1c
   tags = {
     Name = "${var.project}-${var.environment}-rds-sg-ingress-rule"
   }
