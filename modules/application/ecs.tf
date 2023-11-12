9b4671b2e2736c0588c7024ddcd0cade2a2e41c2
GinKuReNai <akito121215@gmail.com>
2023-11-12 17:45:48 +0900

    add: enable container insights

--- modules/application/ecs.tf
+++ modules/application/ecs.tf
@@ -3,6 +3,11 @@
 # ---------------------------------------
 resource "aws_ecs_cluster" "ecs_cluster" {
   name = "${var.project}-${var.environment}-ecs-cluster"
+  setting {
+    name = "containerInsights"
+    value = "enabled"
+  }
+
   tags = {
     Name = "${var.project}-${var.environment}-ecs-cluster"
   }
